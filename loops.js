4816f954cf8f81949b66006ed52399d833976151
Garry Clerge <clergeg@gmail.com>
2017-09-11 17:53:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,16 @@
+function forLoop(array){
+  for(i = 0; i < 25; i++){
+    item = i === 1 ? 'I am ${i} strange loop.' : `I am ${i} strange loops.`;
+    array.push(item)
+  }
+  return array
+}
 
-
+function whileLoop(n){
+  while (n > 0){
+    console.log(--n)
+  }
+}
 
 
 
